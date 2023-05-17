b805b0d5e71dbade1e216413ac5e1aa3d656c9bd
tutruong193 <128039200+tutruong193@users.noreply.github.com>
2023-05-17 14:31:06 +0700

    ok

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
-var port = process.env.PORT || 3001;
-app.listen(port);
+var port = process.env.PORT || 5000;
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -39,5 +39,5 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
-
+app.listen(port);
 module.exports = app;
